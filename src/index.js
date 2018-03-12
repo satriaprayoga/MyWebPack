dc1799bdbaf6e311a9b6cf4db3fc78f6b96938f9
Glenn Bjurestrand <32043740+GlBj@users.noreply.github.com>
2018-03-12 18:03:49 +0100

    Split Bootstrap and updated Babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "bootstrap";
-
-import "./css/main.scss";
+require("./index.html");
+require("./scss/styles.scss");
+require("./js/bootstrap.js");
 
 import fontawesome from "@fortawesome/fontawesome";
 import faGithub from "@fortawesome/fontawesome-free-brands/faGithub";
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
@@ -5,17 +6,30 @@ const extractSass = new ExtractTextPlugin({
   filename: "[name].[contenthash].css",
   disable: process.env.NODE_ENV === "development"
 });
+// Bootstrap dependencies
+const providePluginList = {
+  $: "jquery",
+  jQuery: "jquery",
+  "window.jQuery": "jquery",
+  Popper: ["popper.js", "default"],
+  // Bootstrap scripts
+  Alert: "exports-loader?Alert!bootstrap/js/src/alert",
+  Button: "exports-loader?Button!bootstrap/js/src/button",
+  Carousel: "exports-loader?Carousel!bootstrap/js/src/carousel",
+  Collapse: "exports-loader?Collapse!bootstrap/js/src/collapse",
+  Dropdown: "exports-loader?Dropdown!bootstrap/js/src/dropdown",
+  Modal: "exports-loader?Modal!bootstrap/js/src/modal",
+  Popover: "exports-loader?Popover!bootstrap/js/src/popover",
+  Scrollspy: "exports-loader?Scrollspy!bootstrap/js/src/scrollspy",
+  Tab: "exports-loader?Tab!bootstrap/js/src/tab",
+  Tooltip: "exports-loader?Tooltip!bootstrap/js/src/tooltip",
+  Util: "exports-loader?Util!bootstrap/js/src/util"
+};
 
 module.exports = {
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
         test: /\.html$/,
         use: [
