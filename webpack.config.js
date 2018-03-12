b052c1fd16b6f6abeaeacca0209c589e66cfd8d1
Glenn Bjurestrand <32043740+GlBj@users.noreply.github.com>
2018-03-12 18:21:32 +0100

    Removed never used path

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
