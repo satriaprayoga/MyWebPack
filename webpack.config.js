31fc325e8326681b695045cb5ad0be03331ea903
Glenn Bjurestrand <32043740+GlBj@users.noreply.github.com>
2018-03-16 22:06:16 +0100

    Added require webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
