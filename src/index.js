ebee7f99622a3845b1b62dfef606996adb94b530
Glenn Bjurestrand <32043740+GlBj@users.noreply.github.com>
2018-03-19 15:19:12 +0100

    Remove some test script.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 require("./index.html");
 require("./scss/styles.scss");
-
-// Testing Babel with some ES6
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
